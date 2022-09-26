46d65430b80c29ec6a7cea51cb607d8e708b4595
Niels Huisman <nielsz@pm.me>
2022-09-26 16:14:03 +0200

    sfl8foh

--- main.py
+++ main.py
@@ -5,36 +5,37 @@ import shutil
 import string
 import tensorflow as tf
 import time
+from termcolor import cprint
 
 from tensorflow.keras import layers
 from tensorflow.keras import losses
 print(tf.__version__)
 
 time.sleep (0.5)
-print('=======')
+cprint('=======', "blue")
 print('IMDB Classification AI')
 print('By Dirk & Niels')
-print('=======')
+cprint('=======', "blue")
 time.sleep(1)
 
 if not os.path.isdir('aclImdb'):
-    print("IMDB Database not found, downloading form ai.stanford.edu...")
+    cprint("IMDB Database not found, downloading form ai.stanford.edu...", "yellow")
     # Download dataset
     url = "https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz"
     dataset = tf.keras.utils.get_file("aclImdb_v1", url,untar=True, cache_dir='.', cache_subdir='')
     dataset_dir = os.path.join(os.path.dirname(dataset), 'aclImdb')
 else:
-    print('Dataset already present! Using ./aclImdb')
+    cprint('Dataset already present! Using ./aclImdb', "green")
     dataset_dir = './aclImdb'
 
 train_dir = os.path.join(dataset_dir, 'train')
 
 if os.path.isdir(os.path.join(train_dir, 'unsup')):
-    print('Deleting unsup class reviews...')
+    cprint('Deleting unsup class reviews...', "yellow")
     shutil.rmtree(os.path.join(train_dir, 'unsup'))
-    print('Done!')
+    cprint('Done!', "green")
 
-print('Creating validation set...')
+print('Creating validation set...', "yellow")
 batch_size = 32
 seed = 42
 
